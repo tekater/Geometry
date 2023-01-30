62ac2a454c43c1093343b9590fa50ef3c87593b8
tekater <tekateplay1@yandex.ru>
2023-01-30 21:29:45 +0900

    21:30

--- src/geom/Main.java
+++ src/geom/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Shape shape = new Shape();
         shape.area();
-        
+
         Triangle triangle = new Triangle();
         //triangle.base = 5;
         //triangle.height = 3;
-        System.out.println("Triangle:\n" + "S = b * h/2" + "\n" + triangle.area(5,4) + "\n");
+        System.out.println("Triangle:\n" + "S = b * h/2" + "\nS= " + triangle.area(5,4) + "\n");
         Circle circle = new Circle();
-        System.out.println(String.format("Circle:\n" + "S = PI * R^2\n%.2f\n", circle.area(5)));
+        System.out.println(String.format("Circle:\n" + "S = PI * R^2\nS= %.2f\n ", circle.area(5)));
         Rectangle rectangle = new Rectangle();
-        System.out.println("Rectangle:\n" + "S = w * h" + "\n" + rectangle.area(10,5) + "\n");
+        System.out.println("Rectangle:\n" + "S = w * h" + "\nS= " + rectangle.area(10,5) + "\n");
         Square square = new Square();
-        System.out.println("Square:\n" + "S = s^2" + "\n" + square.area(10));
+        System.out.println("Square:\n" + "S = s^2" + "\nS= " + square.area(10));
     }
 }
